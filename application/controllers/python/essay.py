d60c5664d06a7f6a4bffe506625e4316d4638fd2
marwan-pixel <marwanhadid6@gmail.com>
2024-04-30 13:28:33 +0700

    Update Progress Aplikasi Penilaian Esai

--- application/controllers/python/essay.py
+++ application/controllers/python/essay.py
@@ -1,6 +1,7 @@
 import sys
 import nltk
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from mpstemmer import MPStemmer
 from nltk.corpus import stopwords
 
 def tokenize(jawaban):
@@ -36,9 +37,10 @@ def stemming(jawaban):
 
 if len(sys.argv[1]) > 1:
     sentence = tokenize(sys.argv[1])
+    stemmer = MPStemmer()
     stopwords = set(stopwords.words('indonesian'))
     sentence_without_stopword = [word for word in sentence if not word in stopwords]
     sentence_result = ' '.join(sentence_without_stopword)
-    print(stemming(sentence_result))
+    print(stemmer.stem_kalimat(sentence_result))
 else:
     print('Hello')
